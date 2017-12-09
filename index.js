f8a7b006f91ab395ce17653dbcc58cdd66a882e9
tanshio <rakuburomail+tanshio@gmail.com>
2017-12-09 18:49:39 +0900

    Change main

---
+++ index.js
@@ -0,0 +1,7 @@
+export * from './dist/FixedHeight'
+export * from './dist/changeViewport'
+export * from './dist/jquery-instagram'
+export * from './dist/Countdown'
+export * from './dist/throttle'
+export * from './dist/Countdown'
+
--- typings/main.d.ts
+++
@@ -1,6 +0,0 @@
-export * from './FixedHeight';
-export * from './changeViewport';
-export * from './jquery-instagram';
-export * from './Countdown';
-export * from './throttle';
-export * from './Countdown';
